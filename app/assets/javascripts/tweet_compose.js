f451f9cd4958f0f3159e224439149a1bb467aed4
eva+ron <appacademy.io>
2014-12-19 17:49:42 -0500

    Finishes phase 4

--- app/assets/javascripts/tweet_compose.js
+++ app/assets/javascripts/tweet_compose.js
@@ -3,6 +3,8 @@ $.tweetCompose = function (el) {
   this.tweetSubmit();
   this.$inputs = this.$el.find(":input");
   var charsLeft = 140;
+  this.addUser();
+  this.removeUser();
 
   this.$el.find("textarea").on("keyup", function(event){
     charsLeft = 140;
@@ -29,7 +31,26 @@ $.tweetCompose.prototype.tweetSubmit = function() {
       }
     })
   })
-}
+};
+
+$.tweetCompose.prototype.addUser = function() {
+  var that = this;
+  $('a.add-mentioned-user').on("click", function(event){
+    var $script = that.$el.find(('script'));
+    var $html = $script.html();
+    that.$el.find('ul.mentioned-users').append($html);
+  });
+};
+
+$.tweetCompose.prototype.removeUser = function () {
+  var $ul = this.$el.find('ul.mentioned-users');
+
+  $ul.on("click", "a.remove-mentioned-user", function(event){
+    var div = $(event.currentTarget).parent();
+    div.remove();
+  });
+};
+
 
 $.tweetCompose.prototype.handleSuccess = function(response) {
   var id = this.$el.data("tweets-ul");
@@ -41,12 +62,13 @@ $.tweetCompose.prototype.handleSuccess = function(response) {
   $li.append(tweet);
 
   $ul.prepend($li);
-}
+};
 
 $.tweetCompose.prototype.clearInput = function() {
   this.$el[0].reset();
   this.$el.find("textarea").keyup();
-}
+  this.$el.find('ul.mentioned-users').empty();
+};
 
 
 $.fn.tweetCompose = function () {
